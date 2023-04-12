8fdee84328152715c231595a3c9955eed78b62f8
Laura Comte <comtelaurapro@gmail.com>
2023-04-12 18:42:23 +0200

    ajout js-bases

---
+++ js-bases/script.js
@@ -0,0 +1,31 @@
+
+// let x = 5;
+// let y = 5;
+
+
+
+
+// if (x == y) {
+// alert("youpi!");
+// }
+
+// if (x>y || x>1) {
+//     console.log('OUI');
+// }
+// if (x>y && x>1) {
+//     console.log('OUI');
+// }
+
+function nameSomething() {
+    console.log("je fais un truc");
+    console.log(5+6);
+    alert("calcul terminé");
+}
+
+nameSomething();
+
+const addition = (a, b) => {
+    console.log(a+b);
+}
+
+addition(4, 3);
